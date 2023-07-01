04f874d8957b79ec3e96914069bb257b3396341f
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-06-30 23:27:52 -0300

    feat: implementing form behaviors

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,34 @@
+import { FormEvent, useRef, useState } from 'react';
+import { useParticipant } from '../../hooks/useParticipant';
+import { useErrorMessage } from '../../hooks/useErrorMessage';
+import './form.css';
+
 export const Form = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const addParticipant = useParticipant();
+  const errorMessage = useErrorMessage();
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    addParticipant(name);
+    setName('');
+    inputRef.current?.focus();
+  };
+
   return (
-    <>
-      <input type="text" placeholder="New participant" />
-      <button disabled></button>
-    </>
+    <form onSubmit={handleSubmit}>
+      <input 
+        ref={inputRef}
+        type="text"
+        value={name}
+        onChange={event => setName(event.target.value)} 
+        placeholder="New participant" 
+      />
+      <button disabled={!name}></button>
+
+      {errorMessage && <span role="alert">{errorMessage}</span>}
+    </form>
   );
 };
