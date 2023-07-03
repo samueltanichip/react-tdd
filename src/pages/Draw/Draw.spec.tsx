80dd13e2acc3778ec05426ea4f5c82135c3ceb4f
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-07-03 17:31:15 -0300

    test: adding Draw page tests

---
+++ src/pages/Draw/Draw.spec.tsx
@@ -0,0 +1,66 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+import { Draw } from './Draw';
+
+const mockParticipantList: string[] = ['Ana', 'Catarina', 'Jorel'];
+
+jest.mock('../../hooks/useParticipant', () => {
+  return {
+    useParticipant: jest.fn(() => ({
+      participantList: mockParticipantList
+    }))
+  };
+});
+
+const mockDrawResult = new Map([
+  ['Ana', 'Jorel'],
+  ['Jorel', 'Catarina'],
+  ['Catarina', 'Ana']
+]);
+
+jest.mock('../../hooks/useDrawer', () => {
+  return {
+    useDrawer: jest.fn(() => ({
+      drawResult: mockDrawResult
+    }))
+  };
+});
+
+describe('Draw Page Tests', () => {
+  it('should show each participant in the list of participants', () => {
+    render(
+      <RecoilRoot>
+        <Draw />
+      </RecoilRoot>
+    );
+
+    const options = screen.getAllByRole('option');
+
+    expect(options).toHaveLength(mockParticipantList.length + 1);
+  });
+
+  it('should show the draw result when asked', () => {
+    render(
+      <RecoilRoot>
+        <Draw />
+      </RecoilRoot>
+    );
+
+    const select = screen.getByPlaceholderText('Selecione seu nome');
+    const button = screen.getByRole('button');
+
+    fireEvent.change(select, {
+      target: { value: mockParticipantList[0] }
+    });
+
+    fireEvent.click(button);
+
+    const drawResult = screen.getByRole('alert');
+
+    expect(drawResult).toBeInTheDocument();
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+});
