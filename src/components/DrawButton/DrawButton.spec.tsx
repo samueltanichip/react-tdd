dd6d9cd2d00a6e204af7f409029d732ff942644e
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-07-03 17:29:21 -0300

    test: finalizing DrawButton tests

--- src/components/DrawButton/DrawButton.spec.tsx
+++ src/components/DrawButton/DrawButton.spec.tsx
@@ -1,9 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 import { DrawButton } from './DrawButton';
 
-describe('DrawButton Tests', () => {
-  it('should not start the draw with insufficient participants', () => {
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useNavigate: () => mockNavigate
+  };
+});
+
+let mockParticipantList: string[] = [];
+
+jest.mock('../../hooks/useParticipant', () => {
+  return {
+    useParticipant: jest.fn(() => ({
+      participantList: mockParticipantList
+    }))
+  };
+});
+
+const mockDraw = jest.fn();
+
+jest.mock('../../hooks/useDrawer', () => {
+  return {
+    useDrawer: jest.fn(() => ({
+      draw: mockDraw
+    }))
+  };
+});
+
+describe('DrawButton Tests with insufficient participants', () => {
+  it('should not start the draw', () => {
     render(
       <RecoilRoot>
         <DrawButton />
@@ -13,5 +41,43 @@ describe('DrawButton Tests', () => {
     const button = screen.getByRole('button');
 
     expect(button).toBeDisabled();
+  });
+});
+ 
+describe('DrawButton Tests with sufficient participants', () => {
+  beforeAll(() => {
+    mockParticipantList = ['Ana', 'Catarina', 'Jorel'];
+  });
+
+  it('should be able to start the draw', () => {
+    render(
+      <RecoilRoot>
+        <DrawButton />
+      </RecoilRoot>
+    );
+
+    const button = screen.getByRole('button');
+
+    expect(button).not.toBeDisabled();
+  });
+
+  it('should call the draw function', () => {
+    render(
+      <RecoilRoot>
+        <DrawButton />
+      </RecoilRoot>
+    );
+
+    const button = screen.getByRole('button');
+
+    fireEvent.click(button);
+
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+    expect(mockNavigate).toBeCalledWith('/sorteio');
+    expect(mockDraw).toBeCalledTimes(1);
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
   });
 });
--- src/components/DrawButton/DrawButton.tsx
+++ src/components/DrawButton/DrawButton.tsx
@@ -1,6 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { useParticipant } from '../../hooks/useParticipant';
+import { useDrawer } from '../../hooks/useDrawer';
+
 export const DrawButton = () => {
+  const navigate = useNavigate();
+  const { participantList } = useParticipant();
+  const { draw } = useDrawer();
+
+  const handleClick = () => {
+    draw();
+    navigate('/sorteio');
+  };
+
   return (
-    <button className="draw-button" disabled>
+    <button 
+      className="draw-button" 
+      disabled={participantList.length < 3}
+      onClick={handleClick}
+    >
       Sortear!
     </button>
   );
