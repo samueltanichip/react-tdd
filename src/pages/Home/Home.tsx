c7af620dcd4ba08b6c37e3df2f03876b8b409082
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-07-04 14:29:00 -0300

    feat: adding Heading component in the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,16 @@
 import { DrawButton } from '../../components/DrawButton/DrawButton';
 import { Form } from '../../components/Form/Form';
+import { Heading } from '../../components/Heading/Heading';
 import { ParticipantsList } from '../../components/ParticipantsList/ParticipantsList';
+import './home.css';
 
 export const Home = () => {
   return (
-    <>
+    <section className="home">
+      <Heading title="Liste os participantes do amigo secreto!" />
       <Form />
       <ParticipantsList />
       <DrawButton />
-    </>
+    </section>
   );
 };
